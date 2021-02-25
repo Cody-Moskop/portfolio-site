ccd70bcb6a5de5622797b84c752f726af49a1864
DrZoidburgh <CodyRmoskop@gmail.com>
2021-02-24 21:01:29 -0800

    added links

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import { BrowserRouter, Route, Link } from 'react-router-dom'; 
+import Projects from './Projects.js'; 
+import Articles from './Articles.js'; 
+import About from './About.js'; 
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="navigation">
+        <div className="navigation-sub">
+          <a href="" className="item">Projects</a>
+          <a href="" className="item">Articles</a>
+          <a href="" className="item">About</a>
+        </div>
+      </div>
     </div>
   );
 }
-
+ 
 export default App;
